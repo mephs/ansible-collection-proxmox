1262dcb8f1b93617d9358db20ba36b694596db99
Mikhail Vorontsov <mvorontsov@tuta.io>
2024-05-02 03:57:15 +0300

    change github account and email

--- plugins/doc_fragments/proxmox.py
+++ plugins/doc_fragments/proxmox.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2024, Mikhail Vorontsov (@RusMephist) <mvorontsov@tuta.io>
+# Copyright (c) 2024, Mikhail Vorontsov (@mephs) <mvorontsov@tuta.io>
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
--- plugins/module_utils/proxmox.py
+++ plugins/module_utils/proxmox.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2020, Tristan Le Guern <tleguern at bouledef.eu>
-# Copyright (c) 2024, Mikhail Vorontsov (@RusMephist) <mvorontsov@tuta.io>
+# Copyright (c) 2024, Mikhail Vorontsov (@mephs) <mvorontsov@tuta.io>
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
